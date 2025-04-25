d76d36924d07b75d8e8ffd218e8f4ec79cb953e8
FanHongYu <1159592260@qq.com>
2025-04-25 15:45:03 +0800

    update面试

--- transformer/test.py
+++ transformer/test.py
@@ -5,51 +5,51 @@ import torch.nn.functional as F
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_model, n_head, dropout=0.1):
-        super().__init__()
-
         self.d_model = d_model
-        self.d_k = d_model // n_head
         self.n_head = n_head
+        self.d_k = d_model // n_head
+        
+        self.linear_Q = nn.Linear(d_model, d_model)
+        self.linear_K = nn.Linear(d_model, d_model)
+        self.linear_V = nn.Linear(d_model, d_model)
+
         self.dropout = nn.Dropout(dropout)
 
-        self.q_linear = nn.Linear(d_model, d_model)
-        self.k_linear = nn.Linear(d_model, d_model)
-        self.v_linear = nn.Linear(d_model, d_model)
+        self.output = nn.Linear(d_model, d_model)
 
-        self.out = nn.Linear(d_model, d_model)
-
-    def attention(self, q, k, v, dropout=None, mask=None):
+    def attention(self, q, k, v, mask=None, dropout=None):
         scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(self.d_k)
 
         if mask is not None:
-            mask = torch.unsqueeze(1)
-            scores = scores.masked_fill(mask== 0, -1e6)
+            mask = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask==0, -1e9)
         
-        scores = F.sigmoid(scores, dim=-1)
+        scores = F.softmax(scores, dim=-1)
 
         if dropout is not None:
-            scores = dropout(scores)
+            scores = self.dropout(scores)
+
+        output = torch.matmul(scores, v)
         
-        return scores
-    
+        return output
 
     def forward(self, q, k, v, mask=None):
         bs = q.size(0)
 
-        q = self.q_linear(q).view(bs, -1, self.n_head, self.d_k)
-        k = self.k_linear(k).view(bs, -1, self.n_head, self.d_k)
-        v = self.v_linear(v).view(bs, -1, self.n_head, self.d_k)
+        # (batch_size, seq_length, n_head, d_k)
+        q = self.linear_Q(q).view(bs, -1, self.n_head, self.d_k)
+        k = self.linear_K(k).view(bs, -1, self.n_head, self.d_k)
+        v = self.linear_V(v).view(bs, -1, self.n_head, self.d_k)
 
+        # (batch_size, n_head, seq_length, d_k)
         q = q.transpose(1,2)
         k = k.transpose(1,2)
         v = v.transpose(1,2)
+        
+        scores = self.attention(q, k, v, mask=None, dropout=self.dropout)
 
-        scores = self.attention(q, k, v, self.dropout)
+        concat = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
 
-        scores = scores.transpose(1, 2).contiguous().view(bs, -1, self.d_model)
-
-        output = self.out(scores)
+        output = self.output(concat)
 
         return output
-
-
